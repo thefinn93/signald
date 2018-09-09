81064eb2e321e7f16a8e6ba7a398fc335dd90fa7
Finn Herzfeld <finn@janky.solutions>
2018-09-08 19:14:43 -0700

    OMG i think packaging is working!

--- src/main/resources/io/finn/signald/debian-preinstall.sh
+++ src/main/resources/io/finn/signald/debian-preinstall.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-addgroup signald
+addgroup --system signald
 adduser --system --no-create-home signald
 adduser signald signald  # Add signald user to the signald group
-mkdir -p /var/run/signald
-chown -R :signald /var/run/signald
