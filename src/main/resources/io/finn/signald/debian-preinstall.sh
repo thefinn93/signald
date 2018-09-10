083151907f4398cadf72315a91c6b306d2af6d54
Finn Herzfeld <finn@janky.solutions>
2018-09-09 18:18:16 -0700

    Preinstall now creates a home directory and starts, enables the service

--- src/main/resources/io/finn/signald/debian-preinstall.sh
+++ src/main/resources/io/finn/signald/debian-preinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 addgroup --system signald
-adduser --system --no-create-home signald
+adduser --system --home /var/lib/signald signald
 adduser signald signald  # Add signald user to the signald group
+systemctl enable signald
+systemctl start signald
