1a900776ac74d86ea7b40a6170991256d3197874
Finn Herzfeld <finn@janky.solutions>
2019-03-05 23:40:40 -0800

    Automatically migrade .deb users

--- src/main/resources/io/finn/signald/debian-preinstall.sh
+++ src/main/resources/io/finn/signald/debian-preinstall.sh
@@ -2,6 +2,7 @@
 addgroup --system signald
 adduser --system --home /var/lib/signald signald
 adduser signald signald  # Add signald user to the signald group
+[[ -d /var/lib/signald/.config/signal ]] && mv /var/lib/signald/.config/signal/* /var/lib/signald/ && rm -rf /var/lib/signald/.config/signal
 chown -R signald:signald /var/lib/signald
 systemctl enable signald
 systemctl start signald
