17cc4ae6d0706b2a8f6e7cd8d8e2d98aa16bae3d
Finn Herzfeld <finn@janky.solutions>
2019-12-29 10:45:43 -0800

    fix year in copyright

--- src/main/java/io/finn/signald/JsonSentTranscriptMessage.java
+++ src/main/java/io/finn/signald/JsonSentTranscriptMessage.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018 Finn Herzfeld
+ * Copyright (C) 2019 Finn Herzfeld
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
