f859e2173f601804f1edefe16fe5dadc89ed3041
Finn Herzfeld <finn@janky.solutions>
2020-01-02 18:41:24 -0800

    Update copyright year

--- src/main/java/io/finn/signald/Manager.java
+++ src/main/java/io/finn/signald/Manager.java
@@ -1,5 +1,5 @@
-/**
- * Based on signal-cli, which is copyright (C) 2015 AsamK
+/*
+ * Copyright (C) 2020 Finn Herzfeld
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package io.finn.signald;
 
