dd7d8f41d8156175fd442ac2c5cdda0c3eac1411
Finn Herzfeld <finn@janky.solutions>
2020-08-25 00:08:07 -0700

    quiet UntrustedIdentityExceptions in the logs

--- src/main/java/io/finn/signald/MessageReceiver.java
+++ src/main/java/io/finn/signald/MessageReceiver.java
@@ -21,6 +21,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.signal.libsignal.metadata.SelfSendException;
 import org.whispersystems.libsignal.DuplicateMessageException;
+import org.whispersystems.libsignal.UntrustedIdentityException;
 import org.whispersystems.signalservice.api.messages.SignalServiceContent;
 import org.whispersystems.signalservice.api.messages.SignalServiceEnvelope;
 
@@ -87,6 +88,8 @@ class MessageReceiver implements Manager.ReceiveMessageHandler, Runnable {
               logger.debug("ignoring SelfSendException (see https://gitlab.com/thefinn93/signald/-/issues/24)");
           } else if(exception instanceof DuplicateMessageException || exception.getCause() instanceof DuplicateMessageException) {
               logger.warn("ignoring DuplicateMessageException (see https://gitlab.com/thefinn93/signald/-/issues/50): " + exception.toString());
+          } else if(exception instanceof UntrustedIdentityException) {
+              logger.debug("UntrustedIdentityException", exception.toString());
           } else {
               logger.error("Unexpected error while receiving incoming message! Please report this at " + BuildConfig.ERROR_REPORTING_URL, exception);
           }
