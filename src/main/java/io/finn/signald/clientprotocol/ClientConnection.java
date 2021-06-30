4594416ed8f085f8fbf02f67c4f7d1d15f8a3e1b
Finn <finn@janky.solutions>
2021-06-30 10:28:19 -0700

    line length cleanup

--- src/main/java/io/finn/signald/clientprotocol/ClientConnection.java
+++ src/main/java/io/finn/signald/clientprotocol/ClientConnection.java
@@ -132,8 +132,9 @@ public class ClientConnection implements Runnable {
 
         if (version.equals("v0")) {
           request = mapper.convertValue(rawRequest, JsonRequest.class);
-          logger.debug(
-              "All v0 requests are deprecated and will be removed in late 2021. Client authors, see https://signald.org/articles/protocol-versioning/#deprecation. This message will become a warning in signald 0.15");
+          logger.debug("All v0 requests are deprecated and will be removed in late 2021. Client authors, "
+                       + "see https://signald.org/articles/protocol-versioning/#deprecation. This message will become a "
+                       + "warning in signald 0.15");
           legacySocketHandler.handleRequest(request);
         } else {
           new Request(rawRequest, socket);
