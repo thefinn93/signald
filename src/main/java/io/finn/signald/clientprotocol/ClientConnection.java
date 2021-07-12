d2115d85f24ad6602dea68ed15ef9114430c88ac
Finn <finn@janky.solutions>
2021-07-11 21:01:51 -0700

    Add release notes for 0.14.0

--- src/main/java/io/finn/signald/clientprotocol/ClientConnection.java
+++ src/main/java/io/finn/signald/clientprotocol/ClientConnection.java
@@ -133,7 +133,7 @@ public class ClientConnection implements Runnable {
 
         if (version.equals("v0")) {
           request = mapper.convertValue(rawRequest, JsonRequest.class);
-          logger.debug("All v0 requests are deprecated and will be removed in late 2021. Client authors, "
+          logger.debug("All v0 requests are deprecated and will be removed at the end of 2021. Client authors, "
                        + "see https://signald.org/articles/protocol-versioning/#deprecation. This message will become a "
                        + "warning in signald 0.15");
           legacySocketHandler.handleRequest(request);
