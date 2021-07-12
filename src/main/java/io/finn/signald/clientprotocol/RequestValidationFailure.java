8c56d60e311d4c684ee04a8bda53f4c22e675fd9
Finn <finn@janky.solutions>
2021-07-11 17:15:00 -0700

    Fix how RequestValidationFailure JSON-encodes

--- src/main/java/io/finn/signald/clientprotocol/RequestValidationFailure.java
+++ src/main/java/io/finn/signald/clientprotocol/RequestValidationFailure.java
@@ -17,6 +17,8 @@
 
 package io.finn.signald.clientprotocol;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import io.finn.signald.annotations.ProtocolIgnore;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,5 +34,47 @@ public class RequestValidationFailure extends Exception {
     super("input validation failed, please check the request and try again.");
     validationResults = new ArrayList<>();
     validationResults.add(p);
+  }
+
+  @JsonGetter("stackTrace")
+  @ProtocolIgnore
+  public String getJSONStackTrace() {
+    return null;
+  }
+
+  @JsonGetter("suppressedExceptions")
+  @ProtocolIgnore
+  public String getJSONSuppressedExceptions() {
+    return null;
+  }
+
+  @JsonGetter("suppressed")
+  @ProtocolIgnore
+  public String getJSONSuppressed() {
+    return null;
+  }
+
+  @JsonGetter("ourStackTrace")
+  @ProtocolIgnore
+  public String getJSONOurStackTrace() {
+    return null;
+  }
+
+  @JsonGetter("detailMessage")
+  @ProtocolIgnore
+  public String getJSONDetailMessage() {
+    return null;
+  }
+
+  @JsonGetter("localizedMessage")
+  @ProtocolIgnore
+  public String getJSONLocalizedMessage() {
+    return null;
+  }
+
+  @JsonGetter("cause")
+  @ProtocolIgnore
+  public String getJSONCause() {
+    return null;
   }
 }
