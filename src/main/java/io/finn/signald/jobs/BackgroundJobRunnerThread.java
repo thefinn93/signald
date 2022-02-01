6ddbe63f8f1cfacfa61d1eeedd6b03f3f472b8e4
finn <finn@beeper.com>
2022-01-31 16:47:06 -0800

    add a metric for background job run count

--- src/main/java/io/finn/signald/jobs/BackgroundJobRunnerThread.java
+++ src/main/java/io/finn/signald/jobs/BackgroundJobRunnerThread.java
@@ -7,6 +7,8 @@
 
 package io.finn.signald.jobs;
 
+import io.finn.signald.BuildConfig;
+import io.prometheus.client.Counter;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import org.apache.logging.log4j.LogManager;
@@ -15,6 +17,8 @@ import org.apache.logging.log4j.Logger;
 public class BackgroundJobRunnerThread implements Runnable {
   private static final Logger logger = LogManager.getLogger();
   private static final BlockingQueue<Job> queue = new LinkedBlockingQueue<>();
+  private static final Counter jobsCompleted =
+      Counter.build().name(BuildConfig.NAME + "_background_jobs").help("count of background jobs run").labelNames("job", "error").register();
 
   public static void queue(Job job) { queue.add(job); }
 
@@ -32,9 +36,11 @@ public class BackgroundJobRunnerThread implements Runnable {
       logger.debug("running job {}", job.getClass().getName());
       try {
         job.run();
+        jobsCompleted.labels(job.getClass().getSimpleName(), "").inc();
       } catch (Throwable t) {
         logger.warn("error running {}", job.getClass().getName());
-        logger.debug(t);
+        logger.debug("background job error: ", t);
+        jobsCompleted.labels(job.getClass().getSimpleName(), t.getClass().getCanonicalName()).inc();
       }
     }
   }
