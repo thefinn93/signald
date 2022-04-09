2a045bbc1d433bb471be58b61dab99fcdaf5b58a
inthewaves <inthewaves@pm.me>
2022-04-08 18:19:40 -0700

    add timestamp to banned group member

--- src/main/java/io/finn/signald/clientprotocol/v1/BannedGroupMember.java
+++ src/main/java/io/finn/signald/clientprotocol/v1/BannedGroupMember.java
@@ -1,17 +1,21 @@
 package io.finn.signald.clientprotocol.v1;
 
+import io.finn.signald.annotations.Doc;
 import io.finn.signald.annotations.ExampleValue;
 import org.signal.storageservice.protos.groups.local.DecryptedBannedMember;
 import org.whispersystems.signalservice.api.util.UuidUtil;
 
-/**
- * TODO: Add timestamp when libsignal-service supports it. The proto currently has a timestamp for the Unix millis
- *  of ban timestamp: https://github.com/signalapp/storage-service/commit/8e6da679d95ea6410ab45de03b0b19fc353c763b
- */
 public class BannedGroupMember {
   @ExampleValue(ExampleValue.REMOTE_UUID) public final String uuid;
+  @Doc("Timestamp as milliseconds since Unix epoch of when the user was banned. This field is set by the server.") public final long timestamp;
 
-  public BannedGroupMember(DecryptedBannedMember d) { uuid = UuidUtil.fromByteStringOrUnknown(d.getUuid()).toString(); }
+  public BannedGroupMember(DecryptedBannedMember d) {
+    uuid = UuidUtil.fromByteStringOrUnknown(d.getUuid()).toString();
+    timestamp = d.getTimestamp();
+  }
 
-  public BannedGroupMember(BannedGroupMember otherForCopy) { uuid = otherForCopy.uuid; }
+  public BannedGroupMember(BannedGroupMember otherForCopy) {
+    uuid = otherForCopy.uuid;
+    timestamp = otherForCopy.timestamp;
+  }
 }
