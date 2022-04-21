3bb7e8d2c128681473e324f811cff25e0883b88d
finn <finn@beeper.com>
2022-04-21 12:23:43 -0700

    only uid transition if uid is root

--- uid-transition-entrypoint.sh
+++ uid-transition-entrypoint.sh
@@ -6,17 +6,21 @@
 #
 #
 set -euo pipefail
-
-currentFlag=""
-for arg in $@; do
-  if [[ "${currentFlag}" == "" ]]; then
-    currentFlag="${arg}"
-  else
-    if [[ "${currentFlag}" == "-d" ]]; then
-      echo "Updating permissions on ${arg} (for info see \"migrating from versions before 0.18.0\" on https://signald.org/articles/install/docker/)"
-      chown -R signald:signald "${arg}"
+if [[ "$(id -u)" == "0" ]]; then
+  currentFlag=""
+  for arg in $@; do
+    if [[ "${currentFlag}" == "" ]]; then
+      currentFlag="${arg}"
+    else
+      if [[ "${currentFlag}" == "-d" ]]; then
+        echo "Updating permissions on ${arg} (for info see \"migrating from versions before 0.18.0\" on https://signald.org/articles/install/docker/)"
+        chown -R signald:signald "${arg}"
+      fi
+      currentFlag=""
     fi
-    currentFlag=""
-  fi
-done
-su -c "/bin/entrypoint.sh $*" signald
+  done
+  su -c "/bin/entrypoint.sh $*" signald
+else
+  echo "not chowning signald data directory, if you have permission issues see https://signald.org/articles/install/docker/#migrating-from-versions-before-0180"
+  /bin/entrypoint.sh $*
+fi
